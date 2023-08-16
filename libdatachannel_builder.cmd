1f0fd98652f7e6a3f107942b94034a4be4551a2c
AnshulYbd <anshuly@birddog.tv>
2023-08-17 07:50:48 +1000

    restructure with gcc10 libs

--- libdatachannel_builder.cmd
+++ libdatachannel_builder.cmd
@@ -2,10 +2,14 @@ echo off
 set root_dir=%cd%
 path|find "%root_dir%\BuildTools\7z-win"    >nul || set path=%path%;%root_dir%\BuildTools\7z-win
 
-set GCC_PATH=%root_dir%\toolchains\%GCC_NAME%
+
 set TARGET_ARCH=arm-linux-gnueabihf
-set GCC_NAME=gcc-linaro-7.5.0-2019.12-i686-mingw32_%TARGET_ARCH%
-if exist "%GCC_PATH%" (
+@REM set GCC_NAME=gcc-linaro-7.5.0-2019.12-i686-mingw32_%TARGET_ARCH%
+set GCC_NAME=gcc-arm-10.3-2021.07-mingw-w64-i686-arm-none-linux-gnueabihf
+set GCC_PREFIX=arm-none-linux-gnueabihf
+
+set GCC_REGULAR_PATH=%root_dir%\toolchains\%GCC_NAME%
+if exist "%GCC_REGULAR_PATH%" (
     echo "tool chain path gcc present"
 ) else (
     echo "Downloading tar ball now"
@@ -15,16 +19,19 @@ if exist "%GCC_PATH%" (
         wget https://releases.linaro.org/components/toolchain/binaries/latest-7/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-i686-mingw32_aarch64-linux-gnu.tar.xz
     )
     IF "%TARGET_ARCH%"=="arm-linux-gnueabihf" (
-        wget https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-i686-mingw32_arm-linux-gnueabihf.tar.xz
+        @REM wget https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-i686-mingw32_arm-linux-gnueabihf.tar.xz
+		wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/10.3-2021.07/binrel/gcc-arm-10.3-2021.07-mingw-w64-i686-arm-none-linux-gnueabihf.tar.xz
     )
-	cd %root_dir%\toolchains
-	
 	set EXRACT_BIN="7z.exe"
 	%EXRACT_BIN% x %GCC_NAME%.tar.xz -so | %EXRACT_BIN% x -si -y -ttar
 )
 
+SUBST T: /D
+SUBST T: %GCC_REGULAR_PATH%
+set GCC_PATH=T:
 rm -rf %root_dir%/build
 rm -rf %root_dir%/deps
+cd %root_dir%
 git submodule update --init --recursive --depth 1
 copy /Y .\BuildTools\libsrtp_CMakeLists.txt .\deps\libsrtp\CMakeLists.txt
 copy /Y .\BuildTools\usrsctp_CMakeLists.txt .\deps\usrsctp\CMakeLists.txt
@@ -33,3 +40,5 @@ cmake -B build -DUSE_GNUTLS=0 -DUSE_NICE=0 -G "Unix Makefiles" -DCMAKE_BUILD_TYP
 cd ./build
 cmake --build .
 cd ..
+
+SUBST T: /D
