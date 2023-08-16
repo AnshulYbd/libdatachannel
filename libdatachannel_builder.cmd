f9b9051f22c748849685098a509241e429dc421c
AnshulYbd <anshuly@birddog.tv>
2023-08-16 22:14:19 +1000

    Update libdatachannel_builder.cmd

--- libdatachannel_builder.cmd
+++ libdatachannel_builder.cmd
@@ -1,5 +1,6 @@
 echo off
 set root_dir=%cd%
+path|find "%root_dir%\BuildTools\7z-win"    >nul || set path=%path%;%root_dir%\BuildTools\7z-win
 
 set GCC_PATH=%root_dir%\toolchains\%GCC_NAME%
 set TARGET_ARCH=arm-linux-gnueabihf
@@ -8,20 +9,22 @@ if exist "%GCC_PATH%" (
     echo "tool chain path gcc present"
 ) else (
     echo "Downloading tar ball now"
-mkdir %root_dir%\toolchains
-cd %root_dir%\toolchains
+	mkdir %root_dir%\toolchains
+	cd %root_dir%\toolchains
     IF "%TARGET_ARCH%"=="aarch64-linux-gnu" (
         wget https://releases.linaro.org/components/toolchain/binaries/latest-7/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-i686-mingw32_aarch64-linux-gnu.tar.xz
     )
     IF "%TARGET_ARCH%"=="arm-linux-gnueabihf" (
         wget https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-i686-mingw32_arm-linux-gnueabihf.tar.xz
     )
-	set 7zbin="%root_dir%\BuildTools\7z-win\7z.exe"
-	%7zbin% x %GCC_NAME%.tar.xz -so | %7zbin% x -si -y -ttar
+	cd %root_dir%\toolchains
+	
+	set EXRACT_BIN="7z.exe"
+	%EXRACT_BIN% x %GCC_NAME%.tar.xz -so | %EXRACT_BIN% x -si -y -ttar
 )
 
-rm -rf ./build
-rm -rf ./deps
+rm -rf %root_dir%/build
+rm -rf %root_dir%/deps
 git submodule update --init --recursive --depth 1
 copy /Y .\BuildTools\libsrtp_CMakeLists.txt .\deps\libsrtp\CMakeLists.txt
 copy /Y .\BuildTools\usrsctp_CMakeLists.txt .\deps\usrsctp\CMakeLists.txt
